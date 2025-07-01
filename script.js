5bc4c28e45fe35d7cf39252e604e5bc8d52cbe38
Arthur <arthur.guillaumot@gmail.com>
2025-07-01 16:10:53 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const btn = document.getElementById('test-btn');
+    
+    btn.addEventListener('click', function() {
+        alert('Ça marche !');
+    });
+});
